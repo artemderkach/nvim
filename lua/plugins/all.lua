e01e692f32d4919a508e22adc03be7490358772a
Artem Derkach <artem.derkach.gs@gmail.com>
2024-09-24 14:06:04 +0300

    change glyphs for file explorer

--- lua/plugins/all.lua
+++ lua/plugins/all.lua
@@ -6,6 +6,32 @@ return {
     "nvim-tree/nvim-web-devicons",
   },
   config = function()
-    require("nvim-tree").setup {}
+  require("nvim-tree").setup {
+    renderer = {
+      icons = {
+        show = {
+          git = true,
+	  file = false,
+	  folder = false,
+	  folder_arrow = true,
+        },
+        glyphs = {
+	  folder = {
+	    arrow_closed = "▸",
+	    arrow_open = "▾",
+	  },
+	  git = {
+	    unstaged = "✗",
+	    staged = "✓",
+	    unmerged = "⌥",
+	    renamed = "➜",
+	    untracked = "★",
+	    deleted = "⊖",
+	    ignored = "◌",
+	  },
+        },
+      },
+    },
+  }
   end,
 }
