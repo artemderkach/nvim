500eaaf939d55c91aab3116187187773b6d7ef05
Artem Derkach <artem.derkach.gs@gmail.com>
2024-10-03 19:21:35 +0300

    package upate

--- lua/plugins/all.lua
+++ lua/plugins/all.lua
@@ -1,5 +1,18 @@
 return {
-  { 'nvim-tree/nvim-web-devicons' },
+  {
+   'nvim-treesitter/nvim-treesitter',
+    build = ":TSUpdate",
+    config = function () 
+      local configs = require("nvim-treesitter.configs")
+
+      configs.setup({
+          ensure_installed = { "c", "lua", "vim", "vimdoc", "go", "zig" },
+          sync_install = false,
+          highlight = { enable = true },
+          indent = { enable = true },  
+        })
+    end
+  },
   { 'nvim-telescope/telescope.nvim' },
   { 'neovim/nvim-lspconfig' },
   {
