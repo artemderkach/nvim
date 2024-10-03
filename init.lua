c581cbe1e31c6d5f610a39d334bc14f10cb47a07
Artem Derkach <artem.derkach.gs@gmail.com>
2024-10-03 11:21:28 +0300

    window movement

--- init.lua
+++ init.lua
@@ -10,3 +10,10 @@ require'lspconfig'.zls.setup{}
 
 vim.api.nvim_set_keymap('n', '<leader>e', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
 
+-- movement between windows
+vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', { noremap = true, silent = true })
+
+
